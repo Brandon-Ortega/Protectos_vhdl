8c8758c264da774322e9bee14d75aabbc870e21d
Brandon-Ortega <146219065+Brandon-Ortega@users.noreply.github.com>
2023-09-27 22:42:53 -0500

    OrGate.vhd

--- OrGate.vhd
+++ OrGate.vhd
@@ -19,3 +19,47 @@ architecture behavioral of OrGate is
 		salida <= in1 or in2;
 		
 end architecture;
+
+--Testbench
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity OrGate_tb is
+end entity;
+
+architecture behavior of OrGate_tb is
+    -- Component declaration
+    component OrGate is
+        Port (
+            in1 : in STD_LOGIC;
+            in2 : in STD_LOGIC;
+            salida : out STD_LOGIC
+        );
+    end component;
+
+    -- Signals declaration
+    signal in1_tb, in2_tb, salida_tb : STD_LOGIC;
+
+begin
+    uut: OrGate port map (in1_tb, in2_tb, salida_tb);
+
+    stim_proc: process
+    begin
+        in1_tb <= '0';
+        in2_tb <= '0';
+        wait for 10 ns;
+
+        in1_tb <= '0';
+        in2_tb <= '1';
+        wait for 10 ns;
+		
+        in1_tb <= '1';
+        in2_tb <= '0';
+        wait for 10 ns;
+
+        in1_tb <= '1';
+        in2_tb <= '1';
+        wait for 10 ns;
+        wait;
+    end process;
+end behavior;
